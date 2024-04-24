9b0e5beb64be622d9eff8e441f27a66bc2468a3e
Erick Thamara <erickthamara9@gmail.com>
2024-04-24 10:05:08 +0300

    Completed the birthday reminder challenge

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,29 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const[people,setPeople]=useState(data)
+  const clearList=()=>{
+    setPeople([])
+  }
+
+  return <>
+    <div className='container'>
+
+        <h3>{`${people.length} birthdays today`}</h3>
+        {people.map((person)=>{
+           const{id,name,age,image}=person
+           return(
+             <div className='person' key={id}>
+            <img src={image} alt="Profile Photo" />
+            <h4>{name}</h4>
+            <p>{age}</p>
+           </div>
+           )
+        })}
+        <button onClick={clearList}>CLEAR LIST</button>
+    </div>
+
+  </>;
 }
 
 export default App;

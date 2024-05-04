25c41dd2158db628b3cc542bbf9b85227cc18c60
Erick Thamara <erickthamara9@gmail.com>
2024-05-04 08:43:41 +0300

    Completed grocery bud project which is just a simple to do list

--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,20 +1,28 @@
 import React,{useState} from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
 const List = () => {
-  const [items, setItems] = useState(['cow','goat'])
+  const [items, setItems] = useState([])
   const [grocery, setGrocery] = useState('')
 
   const handleGroceryChange=(e)=>{
     e.preventDefault();
-    console.log(`The value is ${e.target.value}`);
     setGrocery(e.target.value)
   }
   const handleGroceryAddition=()=>{
     if (!items) return
-    console.log(`values are ${grocery}`);
+   
     const listItems=[... items,grocery]
-    console.log(`Items are ${listItems}`);
+
     setItems(listItems)
+    setGrocery('')
+
+  }
+  const deleteGroceryItem=(item)=>{
+    const newList=items.filter((itemName)=>{
+      return itemName !== item
+    })
+    setItems(newList)
+
   }
   return <>
     <div className="form-control">
@@ -24,15 +32,22 @@ const List = () => {
         value={grocery} 
         onChange={handleGroceryChange} />
          <button className="submit-btn" onClick={handleGroceryAddition}>Submit</button>
-    </div>
-    {items.length===0 ? '': <div className="grocery-container">
+    
+    {items.length===0 ? null : <div className="grocery-container">
       {items.map((groceryItem,index)=>{
-        return <div className="grocery-item" key={index}>{groceryItem}</div>
+        return <>
+        <div className="grocery-item">
+         <h4  key={index}>{groceryItem}</h4>
+        <button className='delete-btn' onClick={()=>deleteGroceryItem(groceryItem)}>Delete</button>
+        </div>
+       
+        </>
       })}
-      
-      
-    </div> }
    
+    </div> }
+    {items.length>0 && <button className='clear-btn' onClick={()=>setItems([])}>CLEAR</button>}
+    
+   </div>
   </>
 }
 
